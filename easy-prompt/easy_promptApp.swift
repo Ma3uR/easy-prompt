d1055061a9735c110335e2ec28e38dc0dbe664d4
Andrii M <andriy@veedoo.io>
2025-08-22 00:37:55 +0300

    Initial Commit

---
+++ easy-prompt/easy_promptApp.swift
@@ -0,0 +1,17 @@
+//
+//  easy_promptApp.swift
+//  easy-prompt
+//
+//  Created by Macbook on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct easy_promptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ easy-promptTests/easy_promptTests.swift
@@ -0,0 +1,17 @@
+//
+//  easy_promptTests.swift
+//  easy-promptTests
+//
+//  Created by Macbook on 22.08.2025.
+//
+
+import Testing
+@testable import easy_prompt
+
+struct easy_promptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
